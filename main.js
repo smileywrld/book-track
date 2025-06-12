60443bef2b6adf93ad029694f62e484326363dfd
smileywrld <ismaileyyunusa@gmail.com>
2025-06-12 16:56:54 +0100

    Add initial JavaScript variables and update main container width in CSS

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
-const alert = document.querySelector('.alert');
+const alert = document.querySelector(".alert");
+const libraryForm = document.querySelector(".library-form");
+const inputTitle = document.getElementById("title");
+const entryContainer = document.querySelector(".entry-container");
+const btnSave = document.getElementById("btn-save");
+const btnClearAll = document.querySelector(".clear-all");
