323cd289f4653ae2c2ca244aa8212c1a0834e388
smileywrld <ismaileyyunusa@gmail.com>
2025-06-12 16:51:52 +0100

    Started the Javascript for the this project

--- main.js
+++ main.js
@@ -1,7 +1,1 @@
-// Declaration Section
-const alert = document.querySelector(".alert");
-const libraryForm = document.querySelector(".library-form");
-const inputTitle = document.getElementById("title");
-const entryContainer = document.querySelector(".entry-container");
-const btnSave = document.getElementById("btn-save");
-const btnClearAll = document.querySelector(".clear-all");
+const alert = document.querySelector('.alert');
