63753660bf65fb61458f7f0161d64ed24f0a465f
smileywrld <ismaileyyunusa@gmail.com>
2025-06-12 23:25:31 +0100

    Initialize entry variables for title management and editing state

--- main.js
+++ main.js
@@ -4,3 +4,7 @@ const inputTitle = document.getElementById("title");
 const entryContainer = document.querySelector(".entry-container");
 const btnSave = document.getElementById("btn-save");
 const btnClearAll = document.querySelector(".clear-all");
+
+let entryTitle;
+let editFlag = false;
+let entryTitleID = "";
